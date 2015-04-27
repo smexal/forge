e1e70129b68ea30750c7e77eb3b7bbe497c61392
smexal <silas.maechler@gmail.com>
2015-04-27 21:43:54 +0200

    bug fixed loading icon

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -3,12 +3,16 @@ function setLoading(element, type) {
         element.addClass("loading");
         var loading = $('<img src="../images/default-loading.gif" />');
         element.html(loading);
+        var top = element.outerHeight(true)/2
+        if(top < 50) {
+            top = element.parent().outerHeight(true)/2;
+        }
         loading.css({
             'position' : 'absolute',
             'left': '50%',
             'marginLeft' : loading.width()/2*-1,
-            'top' : element.outerHeight(true)/2,
-            'marginTop' : loading.height()/2*-1
+            'top' : top,
+            'marginTop' : loading.parent().height()/2*-1
         });
     }
 }
