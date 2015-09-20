a8739ccfd7863741209dced43cf7b694372005ab
Silas Mächler <silas.maechler@gmail.com>
2015-09-20 13:08:13 +0200

    test from ubuntu

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 // GETTING PLACES
-define('DOC_ROOT', $_SERVER['DOCUMENT_ROOT'].'/ing-res/');
-define('WWW_ROOT', "/ing-res/");
+define('DOC_ROOT', $_SERVER['DOCUMENT_ROOT'].'/forge/');
+define('WWW_ROOT', "/forge/");
 define('TEMPLATE_DIR', DOC_ROOT."templates/");
 
 // DEVELOPMENTZ
@@ -16,10 +16,9 @@ define('AVAILABLE_LANGUAGES', 'de,en');
 // DATABAZZE
 define('DB_HOST', "localhost");
 define('DB_USER', "root");
-define('DB_PASSWORD', "");
-define('DB_NAME', "ingres");
+define('DB_PASSWORD', "fuckoff");
+define('DB_NAME', "forge");
 
 define('SECURE', false);
-
 
 ?>
