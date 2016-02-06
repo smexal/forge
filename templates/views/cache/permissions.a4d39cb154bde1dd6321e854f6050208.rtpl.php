f5905e2c80c4f5c1d5e29847e4a64e3479c7d87a
Silas Mächler <silas.maechler@gmail.com>
2016-02-06 16:28:23 +0100

    mange

--- templates/views/cache/permissions.a4d39cb154bde1dd6321e854f6050208.rtpl.php
+++ templates/views/cache/permissions.a4d39cb154bde1dd6321e854f6050208.rtpl.php
@@ -1,4 +1,4 @@
-<?php if(!class_exists('raintpl')){exit;}?><div class="maxed padded ajax-reload-container">
+<?php if(!class_exists('raintpl')){exit;}?><div class="maxed ajax-reload-container">
     <div class="row heading-row page-header">
         <div class="col-lg-8"><h1><?php echo $title;?></h1></div>
     </div>
--- views/manage/manage.collections/manage.collections.php
+++ views/manage/manage.collections/manage.collections.php
@@ -5,8 +5,30 @@ class CollectionManagement extends AbstractView {
     public $name = 'collections';
     public $permission = 'manage.collections';
 
+    private $collection = false;
+
     public function content($uri=array()) {
-      Logger::debug($uri);
+      // find out which collection we are editing
+      foreach( $this->app->cm->collections as $collection) {
+        if($collection->getPref('name') == $uri[0]) {
+          $this->collection = $collection;
+          break;
+        }
+      }
+
+      // check if user has permission
+      if(Auth::allowed($collection->permission)) {
+        return $this->app->render(TEMPLATE_DIR."views/sites/", "generic", array(
+            'title' => $this->collection->getPref('all-title'),
+            'content' => $this->manageForm()
+        ));
+      } else {
+        $this->app->redirect("denied");
+      }
+    }
+
+    private function manageForm() {
+      return 'yh';
     }
 }
 
