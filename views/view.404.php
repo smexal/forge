2e4ccae7f6f8bfb62d50a24adf49c18cde5ec12e
Silas Mächler <silas.maechler@gmail.com>
2016-12-10 12:55:26 +0100

    return correct 404 header

--- views/view.404.php
+++ views/view.404.php
@@ -4,6 +4,8 @@ class FourOhFour extends AbstractView {
     public $name = '404';
 
     public function content() {
+        header("HTTP/1.0 404 Not Found");
+        
         return $this->app->render(CORE_TEMPLATE_DIR."views/", "404", array(
             'title' => i('Four Oh! Four'),
             'text' => i('The requested page could not be loaded.')
