2bcd52f232847259704ae5c32c155579399ec2c4
Silas Mächler <smaechler@Raptuss-MacBook-Pro-2.local>
2017-02-03 11:36:28 +0100

    config.php

--- config.php
+++ config.php
@@ -29,7 +29,7 @@ define('AVAILABLE_LANGUAGES', 'de,en');
 // DATABAZZE
 define('DB_HOST', "localhost");
 define('DB_USER', "root");
-define('DB_PASSWORD', "");
+define('DB_PASSWORD', "root");
 define('DB_NAME', "butterlan");
 
 define('SECURE', false);
