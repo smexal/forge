38bc25051455fb8383870546bc6b3e39195e0361
smexal <silas.maechler@gmail.com>
2017-04-09 13:22:18 +0200

    dashboard widgets

--- core/views/manage/view.dashboard.php
+++ core/views/manage/view.dashboard.php
@@ -26,8 +26,18 @@ class DashboardView extends View {
 
         /* welcome message */
         $dash->registerWidget('forge-widget__welcome', [
-            'title' => false,
+            'title' => i('Welcome', 'core'),
             'callable' => [$this, 'welcomeWidget']
+        ]);
+
+        $dash->registerWidget('forge-widget__stats', [
+            'title' => i('Numbers', 'core'),
+            'callable' => [$this, 'statsWidget']
+        ]);
+
+        $dash->registerWidget('forge-widget__quicky', [
+            'title' => i('Quick Access', 'core'),
+            'callable' => [$this, 'quickyWidget']
         ]);
 
         return $dash->render();
@@ -38,6 +48,38 @@ class DashboardView extends View {
         $return.= sprintf(i('Welcome back to Forge, %1$s', 'core'), '<strong>'.App::instance()->user->get('username').'</strong>');
         $return.= '</span>';
         $return.= '<a href="'.Utils::getUrl(["logout"]).'">'.i('Logout', 'core').'</a>';
+        return $return;
+    }
+
+    public function statsWidget() {
+        $return = '<ul class="numbers content-listing">';
+        // amount of pages
+        $pages = count(\Forge\Core\Classes\Pages::getAll());
+        $return.= '<li>'.sprintf(i('%1$s Pages', 'core'), $pages).'</li>';
+
+        // collections
+        $cm = App::instance()->cm;
+        foreach($cm->collections as $collection) {
+            $items = count($collection->items());
+            $return.= '<li>'.sprintf(i('%1$s %2$s'), $items, $collection->getPref('title')).'</li>';
+        }
+
+        // users
+        $users = count(\Forge\Core\Classes\User::getAll());
+        $return.= '<li>'.sprintf(i('%1$s Users', 'core'), $users).'</li>';
+
+
+        $return.= '</ul>';
+        return $return;
+    }
+
+    public function quickyWidget() {
+        $return = '<ul class="content-listing">';
+        $return.= '<li><a href="'.Utils::getUrl(['manage', 'pages']).'">'.i('show and configure pages', 'core').'</a></li>';
+        $return.= '<li><a href="'.Utils::getUrl(['manage', 'users']).'">'.i('add or manage users', 'core').'</a></li>';
+        $return.= '<li><a href="'.Utils::getUrl(['manage', 'string-translation']).'">'.i('manage translations', 'core').'</a></li>';
+        $return.= '<li><a href="'.Utils::getUrl(['manage', 'media']).'">'.i('upload files and media', 'core').'</a></li>';
+        $return.= '</ul>';
         return $return;
     }
 }
