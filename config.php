8b92b3c81b4c62b483f2ec28d997f5157de1ef31
Silas Mächler <silas.maechler@gmail.com>
2017-06-18 00:00:59 +0200

    blaah

--- config.php
+++ config.php
@@ -37,7 +37,7 @@ define('AVAILABLE_LANGUAGES', 'it,de,en');
 @define('DB_HOST', "localhost");
 @define('DB_USER', "root");
 @define('DB_PASSWORD', "root");
-@define('DB_NAME', "robertodonetto-web");
+@define('DB_NAME', "butterlan");
 
 // SEGURIDDY
 define('SECURE', false);
