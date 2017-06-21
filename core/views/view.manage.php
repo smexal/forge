c5d93bac077f979ef7ed6ff9590585670dcd2661
smexal <--global>
2017-06-21 11:41:00 +0200

    user test

--- core/views/view.manage.php
+++ core/views/view.manage.php
@@ -126,7 +126,6 @@ class ManageView extends View {
         if(count($languages) == 1) {
             return '';
         }
-
         $language = Localization::getLanguageInformation(Localization::getCurrentLanguage());
         $available = [];
         foreach($languages as $lang) {
