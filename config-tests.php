c86c3fad8904955df87919f0dc406f7b531a462d
makae <makae90@gmail.com>
2017-09-16 21:36:16 +0200

    Made changes to relations and how the collectionrelations are loaded. Improve phpunit tests for relations. Made changes to utils in phpunit to only provide delete functionality when using butterlan_test. please use the config-tests.php for test specific configurations.

---
+++ config-tests.php
@@ -0,0 +1,8 @@
+<?php
+
+// DO NOT SET THIS IN PRODUCTION OR PAIN IN YOUR PUNY HEART WILL OCCUR
+define('ENV_PHP_UNIT_RUNNING', true);
+@define('DB_HOST', "localhost");
+@define('DB_USER', "root");
+@define('DB_PASSWORD', "root");
+@define('DB_NAME', "butterlan_tests");
