733615aaad8e4a994650284c4bfaf5f3ce752c52
Silas Mächler, MASI3 <silas.maechler@ypsomed.com>
2021-09-30 21:54:22 +0200

    unittests removed

--- config-tests.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// DO NOT SET THIS IN PRODUCTION OR PAIN IN YOUR PUNY HEART WILL OCCUR
-define('ENV_PHP_UNIT_RUNNING', true);
-@define('DB_HOST', "localhost");
-@define('DB_USER', "root");
-@define('DB_PASSWORD', "root");
-@define('DB_NAME', "butterlan_tests");
